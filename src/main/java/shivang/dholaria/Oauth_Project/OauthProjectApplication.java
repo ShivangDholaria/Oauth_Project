bb26377cefd0897f9dd2a53be91e669982bfc179
Shivang Dholaria <dholariashivang@gmail.com>
2024-07-01 16:43:35 -0400

    Initial Commit

---
+++ src/main/java/shivang/dholaria/Oauth_Project/OauthProjectApplication.java
@@ -0,0 +1,13 @@
+package shivang.dholaria.Oauth_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shivang/dholaria/Oauth_Project/OauthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package shivang.dholaria.Oauth_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
